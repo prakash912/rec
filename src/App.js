01aa7fed90074c8244da5a15cf61370bda4ce1ee
Prakash Rai <prakash.rai@symbtechnologies.com>
2022-05-24 21:11:34 +0530

    good

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./App.css";
+import Modal from "./Components/Modal";
+import form2 from "./Components/Forms/form2";
+import heroPic from "./Components/Forms/111.png";
+
+function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <h1>Hey, click on the button to open the modal.</h1>
+      <button
+        className="openModalBtn"
+        onClick={() => {
+          setModalOpen(true);
+        }}
+      >
+        Open
+      </button>
+     
+
+     
+
+      {modalOpen && <Modal setOpenModal={setModalOpen} />}
+      {/* {modalOpen &&  <form2/>} */}
+    </div>
+  );
+}
+
+export default App;
